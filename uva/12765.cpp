cf5fab4d2bdb7a5f140f9600635b4be411748c8c
iMawe <wmedinap@ulasalle.edu.pe>
2018-05-05 14:07:27 -0300

    uva

--- uva/12765.cpp
+++ uva/12765.cpp
@@ -5,43 +5,43 @@ using namespace std;
 int main(){
 	int N, M;
 	while (scanf("%d %d", &N, &M) == 2 && (N != 0 || M != 0)){
-		int fst[10] = {0};
-		int snd[10] = {0};
+		int A[10] = {0};
+		int B[10] = {0};
 		int i, j, a;
 		for (i = 0; i < N; i++){
 			scanf("%d", &a);
 			for (j = 2; j <= a; j++){
-				fst[j]++;
-				//cout<<fst[j]<<" ";
+				A[j]++;
+				//cout<<A[j]<<" ";
 			}
 		}
 		//cout<<endl;
 		for (i = 0; i < M; i++){
 			scanf("%d", &a);
 			for (j = 2; j <= a; j++){
-				snd[j]++;
-				//cout<<snd[j]<<" ";
+				B[j]++;
+				//cout<<B[j]<<" ";
 			}
 			//cout<<endl;
 		}
 		//cout<<endl;
-		fst[2] += 2*fst[4] + fst[6] + 3*fst[8];
-		//cout<<fst[2]<<endl;
-		fst[3] += 2*fst[9] + fst[6];
-		//cout<<fst[3]<<endl;
-		fst[4] = fst[6] = fst[8] = fst[9] = 0;
+		A[2] += 2*A[4] + A[6] + 3*A[8];
+		//cout<<A[2]<<endl;
+		A[3] += 2*A[9] + A[6];
+		//cout<<A[3]<<endl;
+		A[4] = A[6] = A[8] = A[9] = 0;
 
-		snd[2] += 2*snd[4] + snd[6] + 3*snd[8];
-		//cout<<snd[2]<<endl;
-		snd[3] += 2*snd[9] + snd[6];
-		//cout<<snd[3]<<endl;
-		snd[4] = snd[6] = snd[8] = snd[9] = 0;
+		B[2] += 2*B[4] + B[6] + 3*B[8];
+		//cout<<B[2]<<endl;
+		B[3] += 2*B[9] + B[6];
+		//cout<<B[3]<<endl;
+		B[4] = B[6] = B[8] = B[9] = 0;
 
 		j = 1;
 		for (i = 0; i < 10; i++){
 			//cout<<j<<" primero"<<endl;
-			j &= fst[i] == snd[i];
-			//cout<<fst[i]<<" "<<snd[i]<<endl;
+			j &= A[i] == B[i];
+			//cout<<A[i]<<" "<<B[i]<<endl;
 			//cout<<j<<endl;
 		}
 		//cout<<endl;
