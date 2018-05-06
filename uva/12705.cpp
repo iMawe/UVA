2a7123d77f57b508f2da7f34abbf4bddf18ec548
iMawe <wmedinap@ulasalle.edu.pe>
2018-05-06 15:44:29 -0300

    uva

--- uva/12705.cpp
+++ uva/12705.cpp
@@ -7,21 +7,33 @@ using namespace std;
 
 int main(){
 	int value[36]={2,3,3,4,4,4,5,5,5,5,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,9,9,9,9,10,10,10,11,11,12};
-	int j=0,cost=0;
+	int c,cost=0;
 	map<char,int> m;
 	string str;
-	priority_queue<int> vi;
-	for(int i=0; i<str.size(); i++)
-		if(str[i]!=' ')
-			m[str[i]]++;
-	for(auto p:m)
-		vi.push(p.second);
-	
-	while (! vi.empty()){
-		cost+=vi.top()*value[j++];
-		vi.pop();
+	cin>>c;
+	c=c+1;
+	while(c!=0){
+		if(c!=0){
+			c--;
+			priority_queue<int> vi;
+			getline (cin,str);//CALL DEA
+			for(int i=0; i<str.size(); i++)
+				if(str[i]!=' ')
+					m[str[i]]++;
+			for(auto p:m){
+				vi.push(p.second);
+				cout<<vi.top()<<"(1)"<<endl;
+			}
+			int j=0;
+			while (!vi.empty()){
+				cost+=vi.top()*value[j++];
+				cout<<vi.top()<<"(2)"<<endl;
+				vi.pop();
+			}
+			if(cost!=0)
+				cout<<cost<<endl;				
+		}
+		cost=0;
 	}
-	cout<<cost;
-
-	return 0;
+	//return 0;
 }
